5dd84c7365026c7f75273d1d927ed91878907bce
Adel-black <trifonova.alina@mail.ru>
2022-12-09 10:24:36 +1000

    test

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,34 @@
+package ru.netology.selenide;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.locks.Condition;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppCardDeliveryTaskOneTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void taskCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Якутск");
+        String currentDate = generateDate(1, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Иванов Николаевич Пётр");
+        $("[data-test-id='phone'] input").sendKeys("+79991234567");
+        $("[data-test-id='agreement'] input").click();
+        $("button.button").click();
+        $(".notification_content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча забронирована на " + currentDate));
+
+    }
+}
